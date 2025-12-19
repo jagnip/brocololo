36a6ad3f011672ebc646b49cdb10a66466b39abd
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-12-19 16:26:28 +0000

    Update recipe-card.tsx

--- components/recipes/recipe-card.tsx
+++ components/recipes/recipe-card.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 import type { RecipeType } from "@/types/recipe";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { Card, CardHeader } from "../ui/card";
 import { Badge } from "../ui/badge";
 
@@ -11,27 +9,20 @@ type RecipeCardProps = {
 };
 
 export default function RecipeCard({ recipe }: RecipeCardProps) {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(`/?recipe=${recipe.id}`, { scroll: false });
-  };
-
   return (
-    <Card
-      className="cursor-pointer hover:shadow-md transition-shadow"
-      onClick={handleClick}
-    >
-      <CardHeader>
-        <Image
-          src={recipe.photo}
-          alt={recipe.name}
-          width={300}
-          height={300}
-          className="w-full h-auto rounded-xl"
-        />
-        {recipe.name} <Badge>{recipe["hands-on-time"]}</Badge>
-      </CardHeader>
-    </Card>
+    <Link href={`/?recipe=${recipe.id}`} scroll={false}>
+      <Card className="cursor-pointer hover:shadow-md transition-shadow">
+        <CardHeader>
+          <Image
+            src={recipe.photo}
+            alt={recipe.name}
+            width={300}
+            height={300}
+            className="w-full h-auto rounded-xl"
+          />
+          {recipe.name} <Badge>{recipe["hands-on-time"]}</Badge>
+        </CardHeader>
+      </Card>
+    </Link>
   );
 }
