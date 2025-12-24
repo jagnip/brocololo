fc32c27b89e5493194f5d8b462d7c36475e2ea68
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-12-24 10:02:51 +0000

    Updated imports

--- app/recipes/[[...category]]/page.tsx
+++ app/recipes/[[...category]]/page.tsx
@@ -1,8 +1,7 @@
 import GridSkeleton from "@/components/skeletons/grid-skeleton";
 import RecipeGrid from "@/components/recipes/recipe-grid";
 import { Suspense } from "react";
-import { categoriesData } from "@/lib/categories-data";
-import { notFound } from "next/navigation";
+
 
 type PageProps = {
   params: Promise<{ category?: string }>;
