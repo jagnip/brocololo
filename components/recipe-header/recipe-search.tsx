8875e9fbdfecde6bed393cbc2f9fa9f3774f68a0
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-12-29 10:18:29 +0000

    Added searchbox

--- components/recipe-header/recipe-search.tsx
+++ components/recipe-header/recipe-search.tsx
@@ -1,15 +1,12 @@
 import Form from "next/form";
+import { Command, CommandInput } from "../ui/command";
 
 export default function RecipeSearch() {
   return (
-    <Form action="">
-      <input
-        autoComplete="off"
-        id="search"
-        name="q"
-        placeholder="Search in task title or description..."
-        type="search"
-      />
+    <Form action="" className="flex items-center">
+      <Command className="rounded-lg border md:min-w-[450px]">
+        <CommandInput placeholder="Search recipe..." className="h-9" />
+      </Command>
     </Form>
   );
 }
