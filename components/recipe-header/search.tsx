f04a8c6b20f9ec6edcebf2f0431b39a4ca36394f
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-12-30 11:01:26 +0000

    Implemented custom search

--- components/recipe-header/search.tsx
+++ components/recipe-header/search.tsx
@@ -1,36 +1,44 @@
 "use client";
-
-import { useRouter, useSearchParams, useParams } from "next/navigation";
-import { useTransition, useState, useEffect } from "react";
-import { Command, CommandInput } from "../ui/command";
+import { cn } from "@/lib/utils";
 import Form from "next/form";
+import { useRouter, useSearchParams, useParams } from "next/navigation";
+import { useTransition } from "react";
+import SearchStatus from "./search-status";
 
-export default function RecipeSearch() {
+export default function Search() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const params = useParams();
-  const activeCategory = params.category as string;
+  const activeTab = params.category as string;
 
-  const searchQuery = searchParams.get("q") || "";
-  const [inputValue, setInputValue] = useState(searchQuery);
+  const defaultValue = searchParams.get("q") || "";
+
   const [isPending, startTransition] = useTransition();
 
-  useEffect(() => {
-    setInputValue(searchQuery);
-  }, [searchQuery]);
-
   return (
-    <Form action="" className="flex items-center" >
-      <Command className="rounded-lg border md:min-w-[450px]">
-        <CommandInput
-          placeholder="Search recipe..."
-          className="h-9"
-          value={inputValue}
-          onValueChange={(value) => {
+    <Form
+      action=""
+      className="relative flex w-full flex-col gap-2 sm:w-fit"
+      key={activeTab}
+    >
+      <div className="relative">
+        <input
+          autoComplete="off"
+          id="search"
+          name="q"
+          defaultValue={defaultValue}
+          className={cn(
+            "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+            "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
+          )}
+          placeholder="Search in recipes..."
+          type="search"
+          onChange={(e) => {
             const newSearchParams = new URLSearchParams(
               searchParams.toString()
             );
-            newSearchParams.set("q", value);
+            newSearchParams.set("q", e.target.value);
             startTransition(() => {
               router.push(`?${newSearchParams.toString()}`, {
                 scroll: false,
@@ -38,7 +46,8 @@ export default function RecipeSearch() {
             });
           }}
         />
-      </Command>
+        <SearchStatus searching={isPending} />
+      </div>
     </Form>
   );
 }
