f4fe65e123072d89cb3e3e7dd705be5e2719d1bf
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-12-30 11:19:30 +0000

    Search expands full width

--- components/recipe-header/search/index.tsx
+++ components/recipe-header/search/index.tsx
@@ -17,7 +17,7 @@ export default function Search() {
   return (
     <Form
       action=""
-      className="relative flex w-full flex-col gap-2 sm:w-fit"
+      className="relative flex w-full flex-col gap-2 flex-1 min-w-[250px]"
       key={activeTab}
     >
       <div className="relative">
