fc257560140b31dd4412837cb16bf8679800da10
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2026-01-02 15:13:02 +0000

    Added Prisma

---
+++ app/recipes/@modal/(.)[recipe]/page.tsx
@@ -0,0 +1,11 @@
+import RecipeDialogContainer from "@/components/recipes/dialog-container";
+
+type PageProps = {
+  params: Promise<{ recipe: string }>;
+};
+
+export default async function RecipeModalPage({ params }: PageProps) {
+  const { recipe: recipeSlug } = await params;
+  return <RecipeDialogContainer recipeSlug={recipeSlug} />;
+}
+
