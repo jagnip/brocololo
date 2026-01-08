4116df0c462a892cde00eb0ec4e9d9fecf7f9fbc
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2026-01-08 14:28:20 +0000

    Trying force dynamic again

--- app/recipes/create/page.tsx
+++ app/recipes/create/page.tsx
@@ -1,5 +1,7 @@
 import CreateRecipeFormContainer from "@/components/recipes/form-container";
 
+export const dynamic = "force-dynamic";
+
 export default async function CreateRecipeDialogPage() {
   return (
     <div className="max-w-xl mx-auto mt-10">
