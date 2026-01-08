3557f36afefb9479e7b7e4bab1da7c92ef44e5d3
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2026-01-08 14:33:48 +0000

    Removed force dynamic i hate git

--- app/recipes/create/page.tsx
+++ app/recipes/create/page.tsx
@@ -1,7 +1,5 @@
 import CreateRecipeFormContainer from "@/components/recipes/form-container";
 
-export const dynamic = "force-dynamic";
-
 export default async function CreateRecipeDialogPage() {
   return (
     <div className="max-w-xl mx-auto mt-10">
