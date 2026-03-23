03ea1125a80273909429752f32685feacb624463
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2026-03-23 11:06:45 +0000

    fix(recipes): reduce grid columns on lg screens

--- components/recipes/grid.tsx
+++ components/recipes/grid.tsx
@@ -8,7 +8,7 @@ export type RecipeGridProps = {
 
 export default function RecipeGrid({ recipes }: RecipeGridProps) {
   return (
-    <div className="group-has-[[data-pending='true']]:animate-pulse px-4 pb-4 w-full grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+    <div className="group-has-[[data-pending='true']]:animate-pulse px-4 pb-4 w-full grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {recipes.length === 0 ? (
         <RecipeGridEmpty />
       ) : (
