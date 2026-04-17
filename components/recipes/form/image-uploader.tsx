193499468d2eb872a72d176f6554d0a455e7283e
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2026-04-17 09:35:02 +0100

    feat: basics layout

--- components/recipes/form/image-uploader.tsx
+++ components/recipes/form/image-uploader.tsx
@@ -25,7 +25,7 @@ export function ImageUploader({
   } = useImageUpload(value, onChange);
 
   return (
-    <div className="space-y-4">
+    <div className="flex flex-col gap-item">
       <div className="flex gap-4 items-center">
         <Button
           type="button"
